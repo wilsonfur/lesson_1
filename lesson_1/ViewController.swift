326a235a2bbb54d04edb03aeaa1a592abb649c4f
wilson <wilson@aja.com.tw>
2017-10-24 21:13:35 +0800

    pixel art done

--- lesson_1/ViewController.swift
+++ lesson_1/ViewController.swift
@@ -12,11 +12,13 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var StormTrooper: UIView!
     
+    
 
     override func viewDidAppear(_ animated: Bool) {
-        UIView.animate(withDuration: 1.0, animations: {
+        UIView.animate(withDuration: 3.0, animations: {
             self.StormTrooper.alpha = 1
         })
     }
+
 }
 
